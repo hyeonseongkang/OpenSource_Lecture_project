72e0f0024f961385f979845b5da5805c97054d5b
HyeonSeongKang <gt9697@naver.com>
2022-11-05 02:15:04 +0900

    Add Husky

--- index.js
+++ index.js
@@ -9,29 +9,4 @@ try {
 
 console.log(token);
 
-var rtm = new RTMClient(token);
-rtm.start();
-
-var greeting = require("./greeting");
-var square = require("./square");
-
-rtm.on("message", function (message) {
-  var channel = message.channel;
-  var text = message.text;
-
-  if (!isNaN(text)) {
-    square(rtm, text, channel);
-  } else {
-    switch (text) {
-      case "hi":
-        greeting(rtm, channel);
-        break;
-      default:
-        rtm.sendMessage("I am alive", channel);
-    }
-  }
-  // if (text == 'hello')
-  //     rtm.sendMessage('hi', channel);
-  // else
-  //     rtm.sendMessage('what?', channel);
-});
+console.log("hello!!");
