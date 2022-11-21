a4671c5fcf1a9edcf69e0bad292d555c7fe76768
namaek_2 <science4588@gmail.com>
2022-11-21 18:52:58 +0900

    feat: feature#4 testcode 수정

--- deptInfo.js
+++ deptInfo.js
@@ -1,19 +1,29 @@
 const fs = require("fs");
 
-const deptInfo = function (rtm, channel, text) {
+const getDeptInfo = function (text) {
   const data = fs.readFileSync("./dept.txt", "utf8");
   const arr = data.split(/-|\n/);
+  let deptLocated = "no";
 
   for (var i = 0; i < arr.length; i += 2) {
     if (arr[i].trim() === text.trim()) {
-      const deptLocated = arr[i + 1];
-      rtm.sendMessage(deptLocated, channel);
-      return false;
+      deptLocated = arr[i + 1];
+      break;
     }
+  }
+
+  return deptLocated;
+};
+
+const deptInfo = function (rtm, channel, text) {
+  if (getDeptInfo(text) !== "no") {
+    rtm.sendMessage(deptLocated, channel);
+    return false;
   }
   return true;
 };
 
 module.exports = {
   deptInfo,
+  getDeptInfo,
 };
